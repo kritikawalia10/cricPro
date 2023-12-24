2946c5876eea87e29a27b4d46e8dcffc1bdddd96
Kritika Walia <141112950+kritikawalia10@users.noreply.github.com>
2023-12-24 13:25:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,136 @@
+const one = document.querySelector("#one");
+const id1 = document.querySelector("#id1");
+
+one.addEventListener('mouseover', function () {
+    id1.style.display = 'block';
+});
+
+one.addEventListener('mouseleave', function () {
+    id1.style.display = 'none';
+});
+
+
+
+const two = document.querySelector("#two");
+const id2 = document.querySelector("#id2");
+
+two.addEventListener('mouseover', function () {
+    id2.style.display = 'block';
+});
+
+two.addEventListener('mouseleave', function () {
+    id2.style.display = 'none';
+});
+
+
+const three = document.querySelector("#three");
+const id3 = document.querySelector("#id3");
+
+three.addEventListener('mouseover', function () {
+    id3.style.display = 'block';
+});
+
+three.addEventListener('mouseleave', function () {
+    id3.style.display = 'none';
+});
+
+
+const four = document.querySelector("#four");
+const id4 = document.querySelector("#id4");
+
+four.addEventListener('mouseover', function () {
+    id4.style.display = 'block';
+});
+
+four.addEventListener('mouseleave', function () {
+    id4.style.display = 'none';
+});
+
+
+const five = document.querySelector("#five");
+const id5 = document.querySelector("#id5");
+
+five.addEventListener('mouseover', function () {
+    id5.style.display = 'block';
+});
+
+five.addEventListener('mouseleave', function () {
+    id5.style.display = 'none';
+});
+
+
+
+const six = document.querySelector("#six");
+const id6 = document.querySelector("#id6");
+
+six.addEventListener('mouseover', function () {
+    id6.style.display = 'block';
+});
+
+six.addEventListener('mouseleave', function () {
+    id6.style.display = 'none';
+});
+
+
+
+const seven = document.querySelector("#seven");
+const id7 = document.querySelector("#id7");
+
+seven.addEventListener('mouseover', function () {
+    id7.style.display = 'block';
+});
+
+seven.addEventListener('mouseleave', function () {
+    id7.style.display = 'none';
+});
+
+
+
+const eight = document.querySelector("#eight");
+const id8 = document.querySelector("#id8");
+
+eight.addEventListener('mouseover', function () {
+    id8.style.display = 'block';
+});
+
+eight.addEventListener('mouseleave', function () {
+    id8.style.display = 'none';
+});
+
+
+const nine = document.querySelector("#nine");
+const id9 = document.querySelector("#id9");
+
+nine.addEventListener('mouseover', function () {
+    id9.style.display = 'block';
+});
+
+nine.addEventListener('mouseleave', function () {
+    id9.style.display = 'none';
+});
+
+
+
+const ten = document.querySelector("#ten");
+const id10 = document.querySelector("#id10");
+
+ten.addEventListener('mouseover', function () {
+    id10.style.display = 'block';
+});
+
+ten.addEventListener('mouseleave', function () {
+    id10.style.display = 'none';
+});
+
+
+
+const eleven = document.querySelector("#eleven");
+const id11 = document.querySelector("#id11");
+
+eleven.addEventListener('mouseover', function () {
+    id11.style.display = 'block';
+});
+
+eleven.addEventListener('mouseleave', function () {
+    id11.style.display = 'none';
+});
